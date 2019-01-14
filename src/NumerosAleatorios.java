2c525dfe4c2bee35288a283d02bad601d469592c
PC <PC@DESKTOP-BBHGNUV.calero.local>
2019-01-13 21:41:46 -0500

    esta es la primera modificacion

--- src/NumerosAleatorios.java
+++ src/NumerosAleatorios.java
@@ -43,14 +43,14 @@ public class NumerosAleatorios extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setText("Generes número Randómicos");
+        jButton1.setText("Generes número Aleatorios");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
 
-        jButton2.setText("Ordenar");
+        jButton2.setText("Ordenar la serie");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton2ActionPerformed(evt);
